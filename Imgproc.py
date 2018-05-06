5b2237ca0959ee1babaadcfc0ba9470f14152a36
CerealBlue <ns_raghav@hotmail.com>
2018-05-06 15:29:48 +0530

    Updated Imgproc.py to good extent 0.1.2

--- Imgproc.py
+++ Imgproc.py
@@ -1,7 +1,11 @@
  #import the classes from PIL
 from PIL import Image, ImageDraw, ImageFont
+from time import sleep
 
 punc = ['.', '!', '"', '\'', ',', '-', '?', ';', ' ']
+
+version = '0.1.2'
+print ("\n\n"+ str(version) +"\n\n")
 
 class memeFormat:
 	def __init__(self, memeName, memeLocName, memePicFormat):
@@ -12,141 +16,125 @@ class memeFormat:
 		self.picFormat = memePicFormat
 		self.font = 'Roboto-Black.ttf'
 
-	def addSlide(self, name, x, y, linesize, maxlines):
+	def addSlide(self, name, coordinates, linesize, maxlines):
+				
 		self.data[self.counter] = { 	'name' : name,
-					'x' : x,
-					'y' : y,
+					'x' : coordinates[0],
+					'y' : coordinates[1],
 					'linesize' : linesize,
 					'maxlines' : maxlines}
 		self.counter += 1
 
 	def displayMeme(self):
 		display = []
-		display.append(self.name)
-		display.append("\n")
-		
+		display.append( "Meme Name:\t" )
+		display.append( self.name )
+		display.append( "\nNumber of Templates:\t" )
+		display.append( str(self.counter) + "\n\n\n" )
+		count = 1
+	
 		for template in self.data:
-			display.append( "Name:\t" )
+			display.append( str(count) )
+			count += 1
+			display.append( ".\nName:\t" )
 			display.append( self.data[template]['name'] )
-			display.append( "\tMax. Chars:\t" )
+			display.append( "\nMax. Chars:\t" )
 			display.append( str(self.data[template]['linesize'] * self.data[template]['maxlines']) )
-			display.append( "\n" )
+			display.append( "\n_________\n\n" )
 
 		return ''.join(display)
 
-	def doProc(self, template, userText, formatting = None):
-		#userText = userText + ' '		
-		if (len(userText) >= (self.data[template]['linesize'] * self.data[template]['maxlines'])):
-			return ("ERROR: Too Long")
-		TotalLines = []
-		tempWord = []
-		count = 0
+	def memeControl(self, formatting = None):
+		print (self.displayMeme())
 
-		if (formatting != None):
-			for i in range(0, len(userText), self.data[template]['linesize']):
-				TotalLines.append(userText[i:i+self.data[template]['linesize']])
-				
-			print (TotalLines)
+		AllLines = []
 
-			self.printImage(TotalLines, template)
-			return 1
+		for template in range (0, self.counter, 1):
+			print ("\nEnter Template:\t"+str(template+1))
+			userText = str(input()) 
 
+			if (len(userText) >= (self.data[template]['linesize'] * self.data[template]['maxlines'])):
+				return ("ERROR: Too Long")
 		
+			TotalLines = []
+			tempWord = []
+			count = 0
 
-		for i in range(0, len(userText), self.data[template]['linesize']):
-			tempLine = userText[count:count+self.data[template]['linesize']]
-			print (tempLine, type(tempLine))
-			
-			if (len(tempLine) < self.data[template]['linesize']):
-				print (tempLine)
-				TotalLines.append(tempLine)
+			if (formatting != None):
+				for i in range(0, len(userText), self.data[template]['linesize']):
+					TotalLines.append(userText[i:i+self.data[template]['linesize']])
+					
+				print (TotalLines)
 				break
+				#self.printImage(TotalLines, template)
+				#return 1
 
-			if (tempLine[self.data[template]['linesize']-1] not in punc):
-				j = self.data[template]['linesize']-1
-				while (tempLine[j] not in punc):
-					tempLine = tempLine[:-1]
-					print(tempLine)
-					j -= 1
-				i = j
-				count += i+1
-				print (tempLine)
-			else:
-				count+=self.data[template]['linesize']
-			print (len(tempLine))
-			TotalLines.append(tempLine)
-			print(TotalLines)
-		print (TotalLines)
+			while (len(userText) != 0):
+				tempWord = (userText[:self.data[template]['linesize']])
 
-		"""while (len(userText) != 0):
-			tempLine = userText[count:count+self.data[template]['linesize']]
-			print (tempLine, len(tempLine))
-			if (len(tempLine) < self.data[template]['linesize']):
-				TotalLines.append(tempLine)
-				break
-			if (templine[self.data[template]['linesize']-1] not in punc):
-			"""	 
-				
+				if (len(tempWord) < self.data[template]['linesize']):
+					TotalLines.append(''.join(tempWord))
+					userText = ''
+					break
 
+				if (tempWord[self.data[template]['linesize']-1] not in punc):
+					if (userText[self.data[template]['linesize']] in punc):
+						TotalLines.append(''.join(tempWord))
+						userText = userText[self.data[template]['linesize']+1:]
 
-		self.printImage(TotalLines, template)
+					else:
+						enum = 0
+						referrer = self.data[template]['linesize']-1
+
+						while (tempWord[referrer-enum] not in punc):
+							tempWord = tempWord[:-1]							
+							enum += 1	
+
+						userText = userText[referrer-enum+1:]
+						TotalLines.append(''.join(tempWord))
+
+				else:
+					TotalLines.append(''.join(tempWord))
+					userText=userText[self.data[template]['linesize']:]
+
+			AllLines.append(TotalLines)
+		
+		print (AllLines)
+		
+		self.printImage(AllLines)
 		return 1
 			
 
-	def printImage(self, Lines, template):
-		count = 0
-
+	def printImage(self, AllLines):
 		image = Image.open(self.location+"."+self.picFormat)
 		font = ImageFont.truetype(str(self.font), size = 30)
 		color = 'rgb(0,0,0)'
 		
 		draw = ImageDraw.Draw(image)
-
-		for line in Lines:
-			draw.text( (self.data[template]['x'], self.data[template]['y']+(30*count)), str(line), fill=color, font=font)
-			count += 1
+		
+		for template in range(0, self.counter, 1):
+			count = 0
+			for line in AllLines[template]:
+				draw.text( (self.data[template]['x'], self.data[template]['y']+(30*count)), str(line), fill=color, font=font)
+				count += 1
 
 		image.save(self.location+"Modify."+self.picFormat)
 
 
 
-drake = memeFormat("IminDanger", "imInDangerMeme", "png")
-drake.addSlide("Lines", 30, 20, 35, 5)
-#drake.addSlide("DrakeDown", 350, 290, 15, 6)
-print (drake.displayMeme())
-
-userText = str(input("Enter:"))
-
-drake.doProc(0, userText)
+drake = memeFormat("Drake Meme", "DrakeMeme", "jpg")
+drake.addSlide("DrakeTop", (350, 20), 15, 6)
+drake.addSlide("DrakeDown", (350, 290), 15, 6)
 
 
+result = drake.memeControl()
+if (result == 1):
+	print ("Okie Dokie! DONE!")
+else:
+	print (result)
 
-
-
-
-
-
-#create the Image object (inputStarts)
-"""image = Image.open("DrakeMeme.jpg")
-
-#drawing context with image as background
-draw = ImageDraw.Draw(image)
-
-#sizein = int(input())
-
-#font related stuff
-font = ImageFont.truetype("Roboto-Black.ttf", size = 30)
-
-#define color
-color = 'rgb(0,0,0)'
-"""
-
-
-
-
-
-
-
+"""imInDanger = memeFormat("Im In Danger Meme", "imInDanger", "png")
+imInDanger.addSlide("NormalLines", (30, 20), 35, 5)"""
 """data
 
 ##########
